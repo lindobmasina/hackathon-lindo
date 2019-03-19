d4281df13eca1d520ba66ed71af349b70d62210f
Lindokuhle Masina <Lindobmasina@gmail.com>
2019-03-19 15:33:04 +0200

    initial commit

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='test',
+    name='hackathon_lindo',
     version='0.2',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
     description='EDSA example python package',
     long_description=open('README.md').read(),
     install_requires=['numpy'],
-    url='https://github.com/lindobmasina/test',
+    url='https://github.com/lindobmasina/test.git',
     author='Lindokuhle Masina',
     author_email='lindobmasina@gmail.com'
 )
